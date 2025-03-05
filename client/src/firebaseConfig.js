608fcc5cd92d4ead542e4b8c8e560f5b733751b7
parthergk <gauravkumar81464@gmail.com>
2025-03-05 19:22:12 +0530

    firebase auth with login with google

---
+++ client/src/firebaseConfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC-Cl2ptx7fhhizFLEVm5Q6V-iZx08RT2s",
+  authDomain: "letter-14f99.firebaseapp.com",
+  projectId: "letter-14f99",
+  storageBucket: "letter-14f99.firebasestorage.app",
+  messagingSenderId: "444398846180",
+  appId: "1:444398846180:web:79c6d7d9f50c58fc42ecf1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
-const port = 3000 || process.env.PORT
+const port = 3000 || process.env.PORT;
+
+app.post('/auth/google', (req, res)=>{
+    const {token} = req.body();
+    
+})
 
 app.listen(port, ()=>{
     console.log("server is runing on port 3000");
