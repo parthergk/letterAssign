d00c8dd2c1d696595e1fb83e03c83303c2f9ef1a
parthergk <gauravkumar81464@gmail.com>
2025-03-05 22:21:02 +0530

    auth completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,13 +9,29 @@ function App() {
   const loginWithGoogle = async ()=>{
     const result = await signInWithPopup(auth, provider);
     const token = await getIdToken(result.user);
-    await fetch('http://localhost:3000/auth/google', {
-      method: "POST",
-      headers: {
-        "Conetent-Type": "application/json"
-      },
-      body: JSON.stringify({token})
-    })    
+    try {
+      const response = await fetch('http://localhost:3000/auth/google', {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json"
+          },
+          body: JSON.stringify({ token }),
+          credentials: 'include'
+      });
+  
+      if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+  
+      const result = await response.json();
+      console.log(result.message);
+      
+  } catch (error) {
+      console.error("Error during login:", error);
+      alert("Login failed! Please try again.");
+  }
+  
+  
   }
 
   return (
--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const admin = require("firebase-admin");
+const cors= require("cors");
+
+require('dotenv').config();
+
 const app = express();
 const port = 3000 || process.env.PORT;
 
-const admin = require("firebase-admin");
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
+app.use(express.json());
+
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
@@ -10,7 +21,10 @@ admin.initializeApp({
 })
 
 const verifyFirebaseToken = async(req, res, next)=>{
-    const token = req.body.token;
+    const {token} = req.body;
+    if (!token) {
+        return res.status(400).json({ error: "Token is required" });
+    }
     try {
         const verifyedToken = await admin.auth().verifyIdToken(token);
         req.user = verifyedToken;
@@ -20,9 +34,17 @@ const verifyFirebaseToken = async(req, res, next)=>{
     }
 }
 
-app.post('/auth/google', (req, verifyFirebaseToken, res)=>{
-    const {token} = req.body();
+app.post('/auth/google', verifyFirebaseToken, (req, res)=>{
+    const {token} = req.body;
+    const jwt_token = jwt.sign(token, process.env.JWT_SECRET);
 
+    res.cookie("token", jwt_token, {
+        httpOnly: true,
+        secure: false,
+        sameSite: 'lax',
+      });
+  
+      res.status(200).json({ message: "Login successful" });
 })
 
 app.listen(port, ()=>{
